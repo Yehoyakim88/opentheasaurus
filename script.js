9dd3cfcb510ece6e02e1f8491062820fa4e06aa8
Yehoyakim88 <yehoyakim@mail.de>
2022-11-02 17:23:44 +0100

    bug fixed in .js file. fetch url works now

--- script.js
+++ script.js
@@ -2,10 +2,10 @@
 
 
 async function getSynonyms() {
-    let url = 'https://www.openthesaurus.de/synonyme/​search?q=test&format=application/json';
+    let url = 'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
     let response = await fetch(url);
     let responseAsJson = await response.json();
-    await console.log('Response is ', responseAsJson);
+    console.log('Response is ', await responseAsJson);
     let synsets = responseAsJson['synsets'];
     // console.log(synsets);
 }
