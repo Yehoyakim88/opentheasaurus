3ec2c303327683d709f1467c2e78e73c367a42ef
Yehoyakim88 <yehoyakim@mail.de>
2022-11-03 09:11:24 +0100

    output synsets as tables. minor bug still existing.

--- script.js
+++ script.js
@@ -5,11 +5,35 @@ async function getSynonyms() {
     let url = 'https://www.openthesaurus.de/synonyme/search?q=test&format=application/json';
     let response = await fetch(url);
     let responseAsJson = await response.json();
-    console.log('Response is ', await responseAsJson);
+    console.log('Response is ', responseAsJson);
     let synsets = responseAsJson['synsets'];
-    // console.log(synsets);
+    renderSynsets(synsets);
 }
 
+
+function renderSynsets(synsets) {
+    let container = document.getElementById('container');
+    container.innerHTML = /*html*/`
+    <div id="sub-container" class="synsetsOutput"> 
+        <span>Es wurden <b>${synsets.length}</b> Synonym-Sets geladen.</span>
+    </div>`;
+    
+    let subcontainer = document.getElementById('sub-container');
+
+    for (let i = 0; i < synsets.length; i++) {
+        const terms = synsets[i]['terms'];
+        subcontainer.innerHTML += /*htmL*/ `
+        <table id="synset-table-${i}" class="synsetTable">
+        </table>`;
+        const mytable = document.getElementById(`synset-table-${i}`);
+        mytable.innerHTML += `<tr>Synset ${i}</tr>`;
+        for (let j = 0; j < terms.length; j++) {
+            const term = terms[j]['term'];
+            mytable.innerHTML += `<tr>${term}</tr>`;
+            
+        }
+    }
+}
 
 
 
